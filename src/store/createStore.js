3802e3f5bbe6a1278578ad40f2a1886d0aa67ed6
DejonGill <dejonjgill@gmail.com>
2017-03-09 13:51:42 -0800

    begining redux store tests

--- src/store/createStore.js
+++
@@ -1,3 +0,0 @@
-describe('Description Block'() => {
-  
-});
--- test/src/store/reducers/test.currentMovieReducer.js
+++ test/src/store/reducers/test.currentMovieReducer.js
@@ -1,11 +1,12 @@
+import { expect } from 'chai';
+
 describe('current movie reducer', () => {
-  const { expect } = require('chai');
 
   it('should return the current state when given an unknown or undefined action', () => {
     const currentState = {
       currentMovie: 'Raiders of the Lost Ark'
     };
-    expect(currentMovieReducer(currentState).to.deep.equal(currentMovie));
-    expect(currentMovieReducer(currentState, { type: 'RANDOM_ACTION'})).to.deep.equal(curerntState);
+    expect(currentMovieReducer(currentState)).to.deep.equal(currentState);
+    expect(currentMovieReducer(currentState, { type: 'RANDOM_ACTION'})).to.deep.equal(currentState);
   });
 });
