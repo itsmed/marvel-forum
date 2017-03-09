9f4533c6394534282fd1bf56ef60136c20201d1b
DejonGill <dejonjgill@gmail.com>
2017-03-09 14:36:55 -0800

    removed console.log

--- src/store/reducers/reducer.currentMovie.js
+++ src/store/reducers/reducer.currentMovie.js
@@ -1,6 +1,6 @@
 import { SET_CURRENT_MOVIE } from '../actions/types';
 
-const currentMovieReducer = (state, action) => {
+const currentMovieReducer = (state = '', action) => {
   switch(action.type) {
   case SET_CURRENT_MOVIE:
     return Object.assign({}, state, {
