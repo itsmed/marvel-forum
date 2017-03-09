60afb1c1ab673814258b414718b4eea5b37d62a4
DejonGill <dejonjgill@gmail.com>
2017-03-09 14:10:56 -0800

    Added to test

--- test/src/store/reducers/test.currentMovieReducer.js
+++ test/src/store/reducers/test.currentMovieReducer.js
@@ -1,6 +1,9 @@
 import { expect } from 'chai';
 
 import currentMovieReducer from '../../../../src/store/reducers/reducer.currentMovie';
+import {
+  SET_CURRENT_MOVIE
+} from '../../../../src/store/actions/types';
 
 describe('current movie reducer', () => {
 
@@ -10,5 +13,19 @@ describe('current movie reducer', () => {
     };
     expect(currentMovieReducer(currentState)).to.deep.equal(currentState);
     expect(currentMovieReducer(currentState, { type: 'RANDOM_ACTION'})).to.deep.equal(currentState);
+  });
+
+  it('should set the action payload as the new movie when passed the correct action type', () => {
+    const action = {
+      type: SET_CURRENT_MOVIE,
+      payload: 'The Matrix'
+    };
+    const currentState = {
+      currentMovie: ''
+    };
+    const expectedState = {
+      currentMovie: 'The Matrix'
+    };
+    expect(currentMovieReducer(currentState, action)).to.deep.equal(expectedState);
   });
 });
