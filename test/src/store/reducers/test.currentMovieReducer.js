112853b5d50d582003e7cec6cba4c58c3cb7b893
DejonGill <dejonjgill@gmail.com>
2017-03-09 14:41:33 -0800

    added test for resetting state

--- test/src/store/reducers/test.currentMovieReducer.js
+++ test/src/store/reducers/test.currentMovieReducer.js
@@ -8,26 +8,37 @@ import {
 describe('current movie reducer', () => {
 
   it('should return the default state when passed undefined as state', () => {
-      const expectedState = {
-        currentMovie: ''
-      };
       expect(currentMovieReducer(undefined, { type: 'REDUX_INIT' })).to.equal('');
   });
 
-  xit('should return the current state when given an unknown action', () => {
+  it('should return the current state when given an unknown action', () => {
     const currentState = {
       currentMovie: 'Raiders of the Lost Ark'
     };
     expect(currentMovieReducer(currentState, { type: 'RANDOM_ACTION'})).to.deep.equal(currentState);
   });
 
-  xit('should set the action payload as the new movie when passed the correct action type', () => {
+  it('should set the action payload as the new movie when passed the correct action type', () => {
     const action = {
       type: SET_CURRENT_MOVIE,
       payload: 'The Matrix'
     };
     const currentState = {
       currentMovie: ''
+    };
+    const expectedState = {
+      currentMovie: 'The Matrix'
+    };
+    expect(currentMovieReducer(currentState, action)).to.deep.equal(expectedState);
+  });
+
+  it('should set the action payload as the new movie when passed the corret action type when the current movie is already set', () => {
+    const action = {
+      type: SET_CURRENT_MOVIE,
+      payload: 'The Matrix'
+    };
+    const currentState = {
+      currentMovie: 'Wayne\'s World'
     };
     const expectedState = {
       currentMovie: 'The Matrix'
